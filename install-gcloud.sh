9fa560073c40b7e016bb376fff2254dff8891f43
Ubuntu <ubuntu@ip-172-31-88-93.ec2.internal>
2018-06-19 20:51:13 +0000

    .

---
+++ install-gcloud.sh
@@ -0,0 +1,3 @@
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-206.0.0-linux-x86_64.tar.gz
+tar -xvf google-cloud-sdk-206.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
